6715061df13769962db5c8630648d135f415703b
chrxn <psilocene@tuta.io>
2015-11-25 19:31:32 -0700

    tried something else

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-module.exports = {
-    hooks: {
-      {
-        "page:before": function(page) {
-          var query = /(\n[+-]{3}).*(\n[+-]{3})/g
-          page.content = page.content.replace(query, "")
-          return page;
-    }
+var _ = require('lodash');
+
+
+function processContent(_page) {
+  var query = /(\n[+-]{3}).*(\n[+-]{3})/g
+  _page.content = _page.content);
+  return _page;
 }
-    }
+
+module.exports = {
+  hooks: {
+    'page:before': processContent
+  }
 };
